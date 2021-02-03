939260175eb30d00b846ca9b14a72148ab156772
JasonManason <Jasondemey1995@gmail.com>
2021-02-03 19:08:19 +0100

    Kleine aanpassing in de functie fizzbuzz() en functie caesarcijfer() afgerond.

--- Formatieve_opdracht_1.py
+++ Formatieve_opdracht_1.py
@@ -247,20 +247,8 @@ def fibonacci(n, v0=0, v1=1):
 #==============================================Opdracht 11, Caesarcijfer:===============================================
 def caesarcijfer():
     '''Vraagt gebruiker om tekst en een x rotatie en zet de gegeven tekst om in caesarcode met als rotatie x.'''
-
-    # for char in zin: + input uit lijst alfabet if charachter is not een leesteken
-
-    #note: 26 letters. evt modulo?
-
-    # c = (x + n) % 26
-
-    alpha = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-    # check of hoofdletter? if hoofdletter >>> letter.isupper()
-    # eerst kleiner maken voor vergelijken, dan zoeken in alpha lijst en DAN weer letter.capytalize
-
-    # text = input('give a sentence:\n')
-    # shift = input('Give a number to rotate the sentence with:\n')
+    code = ''
+    new_uni = []
 
     while True:
         try:
@@ -271,18 +259,24 @@ def caesarcijfer():
             print('Please enter a valid input.')
             continue
 
+    for i in text:
+        uni = ord(i)
+        uni += rotation
+        new_uni.append(uni)
 
-    print(f'sentence: {text}\nshift: {rotation}')
+    for j in new_uni:
+        code += (chr(j))
 
+    return code
 
-#caesarcijfer()
+#print(f'Your code: {caesarcijfer()}')
 
 
 #================================================Opdracht 12, Fizzbuzz:=================================================
 def fizzbuzz():
     '''Print 1 t/m 100 en geeft voor elke veelvoud van 3 "fizz" en voor elke veelvoud van 5 "buzz".
     In geval van overlap wordt er "fizzbuzz" geprint.'''
-    for i in range(0,100):
+    for i in range(1,101):
         if i % 3 == 0 and i % 5 == 0:
             print('fizzbuzz')
         elif i % 5 == 0:
@@ -292,4 +286,4 @@ def fizzbuzz():
         else:
             print(i)
 
-fizzbuzz()
+#fizzbuzz()
